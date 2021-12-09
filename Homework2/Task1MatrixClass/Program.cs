f038c02bfeded12c23f7e6dda638bae61083db2d
Kęstutis Kravcovas <kekr@gmx.de>
2021-12-09 20:12:07 +0200

    Task1 constructor with params

--- Homework2/Task1MatrixClass/Program.cs
+++ Homework2/Task1MatrixClass/Program.cs
@@ -4,9 +4,42 @@ namespace Task1MatrixClass
 {
     class Program
     {
-        static void Main(string[] args)
+
+		public class Matrix
+		{
+			readonly int Size = 3;
+
+			public Matrix(params int[] numbersdiagonal)
+			{
+
+				int[,] array2D = new int[Size, Size];
+
+				var value = 0;
+				for (int i = 0; i < Size; i++)
+				{
+					for (int j = 0; j < Size; j++)
+					{
+						value = numbersdiagonal[i];
+						if (i == j)
+						{
+							array2D[i, j] = value;
+						}
+						else
+						{
+							array2D[i, j] = 0;
+						}
+
+					}
+				}
+
+				Console.WriteLine(array2D);
+			}
+		}
+		static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+			int[] testNumbers = { 7, 8, 2 };
+			Matrix MatrixTest = new Matrix(testNumbers);
+			Console.WriteLine(testNumbers);
+		}
     }
 }
