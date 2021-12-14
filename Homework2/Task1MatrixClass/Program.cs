124174b503979d8c8bde60b6b3ab0dce225c1507
Kęstutis Kravcovas <kekr@gmx.de>
2021-12-14 15:34:45 +0200

    Class with indexer created

--- Homework2/Task1MatrixClass/Program.cs
+++ Homework2/Task1MatrixClass/Program.cs
@@ -4,42 +4,44 @@ namespace Task1MatrixClass
 {
     class Program
     {
+        public class Matrix
+        {
+            readonly int Size = 3;
+            int[] diagonalArray;
 
-		public class Matrix
-		{
-			readonly int Size = 3;
+            public Matrix(int matrixsize, params int[] diagonalNumbers)
+            {
+                Size = matrixsize;
+                diagonalArray = diagonalNumbers;
 
-			public Matrix(params int[] numbersdiagonal)
-			{
+            }
+            public int this[int i, int j]
+            {
+                get
+                {
+                    if (i == j)
+                    {
+                        return diagonalArray[i];
+                    }
+                    else
+                    {
+                        return 0;
+                    }
+                }
+            }
+        }
 
-				int[,] array2D = new int[Size, Size];
 
-				var value = 0;
-				for (int i = 0; i < Size; i++)
-				{
-					for (int j = 0; j < Size; j++)
-					{
-						value = numbersdiagonal[i];
-						if (i == j)
-						{
-							array2D[i, j] = value;
-						}
-						else
-						{
-							array2D[i, j] = 0;
-						}
-
-					}
-				}
-
-				Console.WriteLine(array2D);
-			}
-		}
 		static void Main(string[] args)
         {
-			int[] testNumbers = { 7, 8, 2 };
-			Matrix MatrixTest = new Matrix(testNumbers);
-			Console.WriteLine(testNumbers);
-		}
+
+            int[] testNumbers = { 9, 6, 3 };
+
+            Matrix TestMatrix = new Matrix(3, testNumbers);
+            Console.WriteLine(TestMatrix[0, 0]);
+            Console.WriteLine(TestMatrix[1, 1]);
+            Console.WriteLine(TestMatrix[2, 2]);
+            Console.WriteLine(TestMatrix[0, 1]);
+        }
     }
 }
