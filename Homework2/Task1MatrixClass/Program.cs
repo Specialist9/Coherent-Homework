26cde457653bfcb8a3298b75ae9baf2e750c0ea7
Kęstutis Kravcovas <kekr@gmx.de>
2021-12-14 19:42:21 +0200

    indexer with conditions

--- Homework2/Task1MatrixClass/Program.cs
+++ Homework2/Task1MatrixClass/Program.cs
@@ -6,28 +6,39 @@ namespace Task1MatrixClass
     {
         public class Matrix
         {
-            readonly int Size = 3;
+            readonly int Size;
             int[] diagonalArray;
 
-            public Matrix(int matrixsize, params int[] diagonalNumbers)
+            public Matrix(int matrixSize, params int[] diagonalNumbers)
             {
-                Size = matrixsize;
+                Size = matrixSize;
                 diagonalArray = diagonalNumbers;
 
+                if ((matrixSize != diagonalNumbers.Length) || (diagonalNumbers == null))
+                {
+                   Size = 0;
+                }
+                Console.WriteLine(Size);
             }
             public int this[int i, int j]
             {
                 get
                 {
-                    if (i == j)
+                    if ((i == j) && (i >= 0) && (i < diagonalArray.Length))
                     {
                         return diagonalArray[i];
                     }
+                    else if(i < 0 || i >= diagonalArray.Length)
+                    {
+                        return 0;
+                    }
+                    
                     else
                     {
                         return 0;
                     }
                 }
+                                
             }
         }
 
@@ -35,13 +46,16 @@ namespace Task1MatrixClass
 		static void Main(string[] args)
         {
 
-            int[] testNumbers = { 9, 6, 3 };
+            int[] testNumbers = { 9, 6, 4 };
+            int testSize = 5;
 
-            Matrix TestMatrix = new Matrix(3, testNumbers);
+            Matrix TestMatrix = new Matrix(testSize, testNumbers);
+
             Console.WriteLine(TestMatrix[0, 0]);
             Console.WriteLine(TestMatrix[1, 1]);
+            Console.WriteLine(TestMatrix[3, 3]);
             Console.WriteLine(TestMatrix[2, 2]);
-            Console.WriteLine(TestMatrix[0, 1]);
+            
         }
     }
 }
