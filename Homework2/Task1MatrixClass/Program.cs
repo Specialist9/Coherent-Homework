e699aeab87e5ec7d2e4c62fd6f79f0efd7667b24
Kęstutis Kravcovas <kekr@gmx.de>
2021-12-15 12:43:41 +0200

    Equals() override

--- Homework2/Task1MatrixClass/Program.cs
+++ Homework2/Task1MatrixClass/Program.cs
@@ -7,7 +7,7 @@ namespace Task1MatrixClass
         public class Matrix
         {
             readonly int Size;
-            int[] diagonalArray;
+            readonly int[] diagonalArray;
 
             public Matrix(int matrixSize, params int[] diagonalNumbers)
             {
@@ -40,6 +40,50 @@ namespace Task1MatrixClass
                 }
                                 
             }
+
+            public int Track()
+            {
+                int diagonalSum = 0;
+
+                for (int i = 0, j = 0; i < diagonalArray.Length; i++, j++)
+                {
+                    if(i == j)
+                    {
+                        diagonalSum += diagonalArray[i];
+                    }
+                    
+                }
+                return diagonalSum;
+            }
+
+                public override bool Equals(object obj)
+                {
+                    var diagMatrix = obj as Matrix;
+
+                if (obj == null || GetType() != obj.GetType() || Size != diagMatrix.Size)
+                {
+                    return false;
+                }
+
+                
+                for(int i = 0; i < diagonalArray.Length; i++)
+                {
+                    if(diagonalArray[i] != diagMatrix.diagonalArray[i])
+                    {
+                        return false;
+                    }
+                }
+                
+                return true;
+                }
+
+                // override object.GetHashCode
+                public override int GetHashCode()
+                {
+                    // TODO: write your implementation of GetHashCode() here
+                    throw new NotImplementedException();
+                    return base.GetHashCode();
+                }
         }
 
 
@@ -47,15 +91,25 @@ namespace Task1MatrixClass
         {
 
             int[] testNumbers = { 9, 6, 4 };
-            int testSize = 5;
+            int testSize = 3;
+
+            int[] testNumbers2 = { 7, 5, 8 };
+            int testSize2 = 3;
 
             Matrix TestMatrix = new Matrix(testSize, testNumbers);
+            Matrix TestMatrix1 = new Matrix(testSize, testNumbers);
+            Matrix TestMatrix2 = new Matrix(testSize2, testNumbers2);
 
             Console.WriteLine(TestMatrix[0, 0]);
             Console.WriteLine(TestMatrix[1, 1]);
             Console.WriteLine(TestMatrix[3, 3]);
             Console.WriteLine(TestMatrix[2, 2]);
-            
+            Console.WriteLine(TestMatrix.Track()+"\n\n");
+
+            Console.WriteLine(TestMatrix.Equals(TestMatrix1));
+            Console.WriteLine(TestMatrix.Equals(TestMatrix2));
+
+
         }
     }
 }
