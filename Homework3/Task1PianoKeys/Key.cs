c4fdff0ae439c9860dd5fd6afca16474c6d7e1ea
Kęstutis Kravcovas <kekr@gmx.de>
2022-01-01 03:01:26 +0200

    ToString() overridden

---
+++ Homework3/Task1PianoKeys/Key.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1PianoKeys
+{
+    struct Key
+    {
+        public Notes Note;
+        public Octaves Octave;
+        public Accidentals? Accidental;
+        public Key (Notes note, Octaves octave, Accidentals accidental)
+        {
+            Note = note;
+            Octave = octave;
+            Accidental = accidental;
+        }
+
+        public Key(Notes note, Octaves octave)
+        {
+            Note = note;
+            Octave = octave;
+            Accidental = null;
+            
+        }
+
+        public override string ToString()
+        {
+            StringBuilder temp = new();
+
+            if(Accidental == null)
+            {
+                temp.Append($"{Note}{(int)Octave}");
+            }
+            else
+            {
+                temp.Append($"{Note}{(char)Accidental}{(int)Octave}");
+
+            }
+
+            return temp.ToString();
+        }
+    }
+
+    public enum Octaves
+    {
+        Zero,
+        First,
+        Second,
+        Third,
+        Fourth,
+        Fifth,
+        Sixth,
+        Seventh,
+        Eighth
+
+    }
+
+    public enum Notes
+    {
+        C,
+        D,
+        E,
+        F,
+        G,
+        A,
+        B
+    }
+
+    public enum Accidentals
+    {
+        Sharp = '#',
+        Flat = 'b'
+    }
+}
--- Homework3/Task1PianoKeys/Program.cs
+++ Homework3/Task1PianoKeys/Program.cs
@@ -6,7 +6,11 @@ namespace Task1PianoKeys
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Key Test1 = new Key(Notes.C, Octaves.First, Accidentals.Sharp);
+            Console.WriteLine(Test1.ToString());
+
+            Key Test2 = new Key(Notes.D, Octaves.First);
+            Console.WriteLine(Test2.ToString());
         }
     }
 }
