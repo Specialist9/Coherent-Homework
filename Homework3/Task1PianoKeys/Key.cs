d5a7e556b1faa9cd5176e70cb2a3e72af3021b6c
Kęstutis Kravcovas <kekr@gmx.de>
2022-01-02 12:07:52 +0200

    Equals() implemented

--- Homework3/Task1PianoKeys/Key.cs
+++ Homework3/Task1PianoKeys/Key.cs
@@ -6,31 +6,39 @@ using System.Threading.Tasks;
 
 namespace Task1PianoKeys
 {
-    struct Key
+    struct Key : IComparable<Key>
     {
         public Notes Note;
         public Octaves Octave;
-        public Accidentals? Accidental;
-        public Key (Notes note, Octaves octave, Accidentals accidental)
+        public Accidentals Accidental;
+
+        public Key (Notes note, Octaves octave, Accidentals accidental = 0)
         {
             Note = note;
             Octave = octave;
             Accidental = accidental;
-        }
 
-        public Key(Notes note, Octaves octave)
-        {
-            Note = note;
-            Octave = octave;
-            Accidental = null;
-            
+            if(Octave == Octaves.Zero)
+            {
+                if(Note != Notes.A || Note != Notes.B)
+                {
+                    throw new ArgumentException("Octave 0 has only notes A, B");
+                }
+            }
+            else if(Octave == Octaves.Eighth)
+            {
+                if(Note != Notes.C)
+                {
+                    throw new ArgumentException("Octave 8 has only note C");
+                }
+            }
         }
 
         public override string ToString()
         {
             StringBuilder temp = new();
 
-            if(Accidental == null)
+            if(Accidental == 0)
             {
                 temp.Append($"{Note}{(int)Octave}");
             }
@@ -41,6 +49,54 @@ namespace Task1PianoKeys
             }
 
             return temp.ToString();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if(!(obj is Key))
+            {
+                return false;
+            }
+            
+            var testKey = (Key)obj;
+
+            if(this.Octave == testKey.Octave)
+            {
+                if(this.Accidental == Accidentals.Sharp && testKey.Accidental == Accidentals.Flat)
+                {
+                    if ( (this.Note == Notes.A && testKey.Note == Notes.B) ||
+                         (this.Note == Notes.C && testKey.Note == Notes.D) ||
+                         (this.Note == Notes.D && testKey.Note == Notes.E) ||
+                         (this.Note == Notes.F && testKey.Note == Notes.G) ||
+                         (this.Note == Notes.G && testKey.Note == Notes.A)  )
+                    {
+                        return true;
+                    }
+
+                }
+                else if(this.Accidental == Accidentals.Flat && testKey.Accidental == Accidentals.Sharp)
+                {
+                    if ( (this.Note == Notes.B && testKey.Note == Notes.A) ||
+                         (this.Note == Notes.D && testKey.Note == Notes.C) ||
+                         (this.Note == Notes.E && testKey.Note == Notes.D) ||
+                         (this.Note == Notes.G && testKey.Note == Notes.F) ||
+                         (this.Note == Notes.A && testKey.Note == Notes.G)  )
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public int CompareTo(Key other)
+        {
+            throw new NotImplementedException();
         }
     }
 
@@ -71,6 +127,7 @@ namespace Task1PianoKeys
 
     public enum Accidentals
     {
+        None = 0,
         Sharp = '#',
         Flat = 'b'
     }
--- Homework3/Task1PianoKeys/Program.cs
+++ Homework3/Task1PianoKeys/Program.cs
@@ -11,6 +11,11 @@ namespace Task1PianoKeys
 
             Key Test2 = new Key(Notes.D, Octaves.First);
             Console.WriteLine(Test2.ToString());
+
+            Key Test3 = new Key(Notes.D, Octaves.First, Accidentals.Flat);
+            Console.WriteLine(Test3.ToString());
+
+            Console.WriteLine(Test3.Equals(Test1));
         }
     }
 }
