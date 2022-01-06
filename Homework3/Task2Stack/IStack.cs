92bdb5e27fcf39a6adce11ccc256a377ab1fac62
Kęstutis Kravcovas <kekr@gmx.de>
2022-01-06 11:02:26 +0200

    stack / istack created

---
+++ Homework3/Task2Stack/IStack.cs
@@ -0,0 +1,12 @@
+﻿namespace Task2Stack
+{
+    public interface IStack<T>
+    {
+        int Count { get; }
+        bool IsEmpty { get; }
+
+        T Peek();
+        T Pop();
+        void Push(T item);
+    }
+}
