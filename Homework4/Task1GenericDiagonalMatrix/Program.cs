c3c4523f2f44ce5ec0ba2b68538bf2792eae3ea8
Kęstutis Kravcovas <kekr@gmx.de>
2022-01-22 12:57:32 +0200

    blank solution created

---
+++ Homework4/Task1GenericDiagonalMatrix/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task1GenericDiagonalMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
