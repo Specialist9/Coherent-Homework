c13a63e1e4b4fd6156704efd0e1cebc96a724b4d
Kęstutis Kravcovas <kekr@gmx.de>
2022-01-23 13:52:24 +0200

    event implemented

---
+++ Homework4/Task1GenericDiagonalMatrix/ElementChangedEventArgs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1GenericDiagonalMatrix
+{
+    public class ElementChangedEventArgs<T> : EventArgs
+    {
+        public T OldValue;
+        public T NewValue;
+        public int ElementIndex;
+
+        public ElementChangedEventArgs(T oldValue, T newValue, int elementIndex)
+        {
+            OldValue = oldValue;
+            NewValue = newValue;
+            ElementIndex = elementIndex;
+        }
+    }
+}
--- Homework4/Task1GenericDiagonalMatrix/Program.cs
+++ Homework4/Task1GenericDiagonalMatrix/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Task1GenericDiagonalMatrix
 {
@@ -6,7 +8,31 @@ namespace Task1GenericDiagonalMatrix
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                Matrix<string> Matrix1 = new(2);
+                Matrix1.ElementChanged += matrix_ElementChanged;
+                Matrix1[1, 1] = "abc";
+                Matrix1[1, 1] = "def";
+                Matrix1[1, 1] = "def";
+                Matrix1[0, 0] = "xyz";
+                Matrix1[0, 1] = "mno";
+
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Matrix size can't be negative");
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Invalid matrix index");
+            }
+
+            void matrix_ElementChanged(object sender, ElementChangedEventArgs<string> e)
+            {
+                Console.WriteLine($"Matrix element [{e.ElementIndex}, {e.ElementIndex}] changed from {e.OldValue} to {e.NewValue}");
+            }
+            
         }
     }
 }
