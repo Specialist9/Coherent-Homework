72c2d1cf91b4a3528b6de8c70655a6748bf87ca8
Kęstutis Kravcovas <kekr@gmx.de>
2022-01-30 21:57:13 +0200

    indexer implemented

---
+++ Homework5/Task1SparseMatrix/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Task1SparseMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SparseMatrix SpMatrix1 = new(3, 3);
+            SpMatrix1[2, 2] = 27;
+            Console.WriteLine(SpMatrix1[2, 2]);
+        }
+    }
+}
---
+++ Homework5/Task1SparseMatrix/SparseMatrix.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1SparseMatrix
+{
+    class SparseMatrix: IEnumerable<int>
+    {
+        int Rows { get; set; }
+        int Columns { get; set; }
+        public Dictionary<int, int> MatrixElements { get; private set; } = new();
+
+        public SparseMatrix(int rows, int columns)
+        {
+            Rows = rows > 0 ? rows : 1;
+            Columns = columns > 0 ? columns : 1; ;
+        }
+
+        public int this[int row, int column]
+        {
+            get
+            {
+                int index = row * Columns + column;
+                int result;
+                MatrixElements.TryGetValue(index, out result);
+                return result;
+            }
+
+            set
+            {
+                int index = row * Columns + column;
+                MatrixElements[index] = value;
+            }
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
