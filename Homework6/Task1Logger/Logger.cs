6058e466cb76274b71c7b99f54ae05956b920c06
Kęstutis Kravcovas <kekr@gmx.de>
2022-02-11 22:14:55 +0200

    task classes & attributes created

---
+++ Homework6/Task1Logger/Logger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1Logger
+{
+    class Logger
+    {
+        public Logger(string fileName)
+        {
+
+        }
+
+        public void Track(object test)
+        {
+
+        }
+    }
+}
---
+++ Homework6/Task1Logger/PassedObject.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1Logger
+{
+    [TrackingEntity]
+    class PassedObject
+    {
+        [TrackingProperty("Name")]
+        string Name { get; }
+        int Age { get; }
+
+        [TrackingProperty("Phone number")]
+        string PhoneNumber { get; set; }
+        string EmailAddress { get; set; }
+
+        public PassedObject(string name, int age, string phone, string email)
+        {
+            Name = name;
+            Age = age;
+            PhoneNumber = phone;
+            EmailAddress = email;
+        }
+
+        public int GetYearOfBirth()
+        {
+            int dob = (int)DateTime.Today.Year - Age;
+            return dob;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder temp = new();
+            temp.Append($"Person Name: {Name}, Age: {Age}, Phone Nr.: {PhoneNumber}, Email: {EmailAddress}");
+            return temp.ToString();
+        }
+    }
+}
